c869d1bdcb8dc6e8e500389a43cc1f13a0cb4ddb
Saiteja141 <150420449+Saiteja141@users.noreply.github.com>
2023-11-10 12:35:35 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('Family details')
 streamlit.header('menu')
-streamlit.text(1'Father')
-streamlit.text(2'Mother')
-streamlit.text(3'Brother')
+streamlit.text(1 'Father')
+streamlit.text(2 'Mother')
+streamlit.text(3 'Brother')
