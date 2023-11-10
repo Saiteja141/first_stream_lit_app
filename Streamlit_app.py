d0674faefb68852c28433070661b441f4265f3ff
Saiteja141 <150420449+Saiteja141@users.noreply.github.com>
2023-11-10 12:48:12 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('Menu details')
 streamlit.header('menu price')
-streamlit.text(🥣 'Veg 90')
-streamlit.text(🥗 'CB 150')
-streamlit.text(🐔 'Chicken 120')
+streamlit.text('🥣 Veg 90')
+streamlit.text('🥗 CB 150')
+streamlit.text('🐔 Chicken 120')
  
