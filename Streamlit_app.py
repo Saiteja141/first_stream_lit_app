9f8ee9a1ec6c9c194a37044b99b5e61037e78bdd
Saiteja141 <150420449+Saiteja141@users.noreply.github.com>
2023-11-10 12:41:48 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('Family details')
-streamlit.header('menu','age')
+streamlit.header('menu age')
 streamlit.text('Father 60')
 streamlit.text('Mother 50')
 streamlit.text('Brother 30')
